e8444f7843a14d0b09c2e4f2e366671094aaaf0d
Benoit Hiller <benoit.hiller@gmail.com>
2026-07-29 15:04:41 -0400

    Prevent returning nonsense value

--- lib/report_print/dsl.rb
+++ lib/report_print/dsl.rb
@@ -10,10 +10,12 @@ module ReportPrint
     # * `color:` specifies whether the text should render colored output or
     #   not. The default value of `:auto` indicates that it should use the
     #   value of `output.tty?` to determine if color output is enabled or not.
-    def rp(object, output: $>, color: :auto) # :doc:
+    def rp(object, output: $>, color: :auto)
       printer = Printer.new(output, color:)
       printer.rp(object)
       output.write("\n")
+
+      nil
     end
   end
 end
